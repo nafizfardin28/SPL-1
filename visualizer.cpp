e57058a21a3d386ff9273b115d91dc55d15bc0cc
nafizfardin28 <bsse1528@iit.du.ac.bd>
2025-01-22 10:37:50 +0600

    Updated visualizer.cpp

--- visualizer.cpp
+++ visualizer.cpp
@@ -8,50 +8,57 @@ using namespace std;
 void visualizemetrics(const vector<FileMetrics>& allMetrics)
 {
     cout << "CODE SIZE METRICS-" << endl << endl;
-    cout << setw(12) << "FileName" << setw(15)
-         << "TotalLines" << setw(12)
-         << "CodeLines" << setw(12)
-         << "BlankLines  " << setw(14)
-         << "TotalCommentLines" << setw(13)
-         << "Function  " << setw(13)
-         << "MultiCommentLines" << setw(15)
-         << "Comment Ratio";
+    cout << setw(20) << left << "FileName"
+         << setw(15) << "TotalLines"
+         << setw(15) << "CodeLines"
+         << setw(15) << "BlankLines"
+         << setw(20) << "TotalCommentLines"
+         << setw(15) << "Functions"
+         << setw(20) << "MultiCommentLines"
+         << setw(15) << "Comment Ratio (%)";
     cout << endl;
-    cout << string(125, '-') << endl;
+    cout << string(140, '-') << endl;
+
     for (const auto& fileMetrics : allMetrics)
     {
-        cout << setw(12) << fileMetrics.filename.substr(0, 17)
-             << setw(10) << fileMetrics.metrics.totalLines
-             << setw(13) << (fileMetrics.metrics.codeLines - fileMetrics.metrics1.singleLineComments)
-             << setw(13) << fileMetrics.metrics.blankLines
-             << setw(16) << fileMetrics.metrics1.totalCommentLines 
+        cout << setw(20) << left << fileMetrics.filename
+             << setw(15) << fileMetrics.metrics.totalLines
+             << setw(15) << (fileMetrics.metrics.codeLines - fileMetrics.metrics1.singleLineComments)
+             << setw(15) << fileMetrics.metrics.blankLines
+             << setw(20) << fileMetrics.metrics1.totalCommentLines
              << setw(15) << fileMetrics.metrics.functionCount
-             << setw(13) << fileMetrics.metrics1.multiLineComments
-             << setw(18) << fixed << setprecision(2) 
-             << (double)(fileMetrics.metrics1.totalCommentLines) / (fileMetrics.metrics.totalLines) * 100 << "%";
+             << setw(20) << fileMetrics.metrics1.multiLineComments
+             << setw(15) << fixed << setprecision(2)
+             << ((fileMetrics.metrics.totalLines > 0)
+                 ? (static_cast<double>(fileMetrics.metrics1.totalCommentLines) / fileMetrics.metrics.totalLines) * 100
+                 : 0.0) << "%";
         cout << endl;
     }
-    
+
     cout << endl << "TOKEN METRICS-" << endl << endl;
-    cout << setw(12) << "FileName" << setw(13)
-         << "keyWords" << setw(14) 
-         << "Identifiers" << setw(14)
-         << "Operators" << setw(14)
-         << "Numbers" << setw(14)
-         << "Delimiters" << setw(12)
-         << "Unknowns";
+    cout << setw(20) << left << "FileName"
+         << setw(15) << "Keywords"
+         << setw(15) << "Identifiers"
+         << setw(15) << "Operators"
+         << setw(15) << "Numbers"
+         << setw(15) << "Delimiters"
+         << setw(15) << "Unknowns";
     cout << endl;
-    cout << string(95, '-') << endl;
+    cout << string(110, '-') << endl;
+
     for (const auto& fileMetrics : allMetrics)
     {
-        cout << setw(12) << fileMetrics.filename.substr(0, 17)
-             << setw(10) << fileMetrics.metrics2.keyWordCount
-             << setw(13) << fileMetrics.metrics2.identifierCount
-             << setw(13) << fileMetrics.metrics2.operatorCount
-             << setw(13) << fileMetrics.metrics2.numberCount
-             << setw(13) << fileMetrics.metrics2.delimiterCount
-             << setw(13) << fileMetrics.metrics2.unknownCount;
+        cout << setw(20) << left << fileMetrics.filename
+             << setw(15) << fileMetrics.metrics2.keyWordCount
+             << setw(15) << fileMetrics.metrics2.identifierCount
+             << setw(15) << fileMetrics.metrics2.operatorCount
+             << setw(15) << fileMetrics.metrics2.numberCount
+             << setw(15) << fileMetrics.metrics2.delimiterCount
+             << setw(15) << fileMetrics.metrics2.unknownCount;
         cout << endl;
     }
+
     cout << endl;
+}
+
 }
