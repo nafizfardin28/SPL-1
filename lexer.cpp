c2f7e18e9bebcb2b07033538dcac5caa0548eb0e
nafizfardin28 <bsse1528@iit.du.ac.bd>
2025-01-24 22:43:36 +0600

    Updated lexer.cpp

--- lexer.cpp
+++ lexer.cpp
@@ -2,113 +2,148 @@
 #include <cctype>
 #include <unordered_set>
 
-bool isDelimiter(char c) {
+bool isDelimiter(char c)
+{
     static const std::unordered_set<char> delimiters = {
-        '(', ')', '{', '}', '[', ']', ';', ',', '.', ':', '?'
-    };
+        '(', ')', '{', '}', '[', ']', ';', ',', '.', ':', '?'};
     return delimiters.find(c) != delimiters.end();
 }
 
-bool isOperator(char c) {
+bool isOperator(char c)
+{
     static const std::unordered_set<char> operators = {
-        '+', '-', '*', '/', '%', '=', '<', '>', '!', '&', '|', '^', '~'
-    };
+        '+', '-', '*', '/', '%', '=', '<', '>', '!', '&', '|', '^', '~'};
     return operators.find(c) != operators.end();
 }
 
-bool isKeyword(const std::string& str) {
+bool isKeyword(const std::string &str)
+{
     static const std::unordered_set<std::string> keywords = {
-        "main","auto", "break", "case", "char", "const", "continue", "default", "do",
+        "main", "auto", "break", "case", "char", "const", "continue", "default", "do",
         "double", "else", "enum", "extern", "float", "for", "goto", "if",
         "int", "long", "register", "return", "short", "signed", "sizeof", "static",
-        "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
-    };
+        "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"};
     return keywords.find(str) != keywords.end();
 }
 
-TokenCount tokenizeAndCount(const std::string& code) {
+TokenCount tokenizeAndCount(const std::string &code)
+{
     TokenCount count = {0, 0, 0, 0, 0, 0};
     std::string currentToken;
     bool inString = false;
     bool inChar = false;
     bool inComment = false;
 
-    for (size_t i = 0; i < code.length(); ++i) {
+    for (size_t i = 0; i < code.length(); ++i)
+    {
         char c = code[i];
         char nextC = (i + 1 < code.length()) ? code[i + 1] : '\0';
 
-        if (inComment) {
-            if (c == '*' && nextC == '/') {
+        if (inComment)
+        {
+            if (c == '*' && nextC == '/')
+            {
                 inComment = false;
                 i++;
             }
             continue;
         }
-        if (c == '/' && nextC == '*') {
+        if (c == '/' && nextC == '*')
+        {
             inComment = true;
             i++;
             continue;
         }
 
-        if (c == '/' && nextC == '/') {
-            while (i < code.length() && code[i] != '\n') i++;
+        if (c == '/' && nextC == '/')
+        {
+            while (i < code.length() && code[i] != '\n')
+                i++;
             continue;
         }
 
-        if (inString) {
-            if (c == '"' && code[i - 1] != '\\') inString = false;
+        // Handle string literals
+        if (inString)
+        {
+            if (c == '"' && code[i - 1] != '\\')
+                inString = false;
             continue;
         }
-        if (c == '"') {
+        if (c == '"')
+        {
             inString = true;
             continue;
         }
 
-        
-        if (inChar) {
-            if (c == '\'' && code[i - 1] != '\\') inChar = false;
+        if (inChar)
+        {
+            if (c == '\'' && code[i - 1] != '\\')
+                inChar = false;
             continue;
         }
-        if (c == '\'') {
+        if (c == '\'')
+        {
             inChar = true;
             continue;
         }
 
-        if (std::isspace(c) || isDelimiter(c) || isOperator(c)) {
-            if (!currentToken.empty()) {
-                if (isKeyword(currentToken)) {
+        if (std::isspace(c) || isDelimiter(c) || isOperator(c))
+        {
+            if (!currentToken.empty())
+            {
+                if (isKeyword(currentToken))
+                {
                     count.keyWordCount++;
-                } else if (std::isalpha(currentToken[0]) || currentToken[0] == '_') {
+                }
+                else if (std::isalpha(currentToken[0]) || currentToken[0] == '_')
+                {
                     count.identifierCount++;
-                } else if (std::isdigit(currentToken[0])) {
+                }
+                else if (std::isdigit(currentToken[0]))
+                {
                     count.numberCount++;
-                } else {
+                }
+                else
+                {
                     count.unknownCount++;
                 }
                 currentToken.clear();
             }
 
-            if (isDelimiter(c)) {
+            if (isDelimiter(c))
+            {
                 count.delimiterCount++;
-            } else if (isOperator(c)) {
+            }
+            else if (isOperator(c))
+            {
                 count.operatorCount++;
             }
             continue;
         }
+
         currentToken += c;
     }
 
-    if (!currentToken.empty()) {
-        if (isKeyword(currentToken)) {
+    if (!currentToken.empty())
+    {
+        if (isKeyword(currentToken))
+        {
             count.keyWordCount++;
-        } else if (std::isalpha(currentToken[0]) || currentToken[0] == '_') {
+        }
+        else if (std::isalpha(currentToken[0]) || currentToken[0] == '_')
+        {
             count.identifierCount++;
-        } else if (std::isdigit(currentToken[0])) {
+        }
+        else if (std::isdigit(currentToken[0]))
+        {
             count.numberCount++;
-        } else {
+        }
+        else
+        {
             count.unknownCount++;
         }
     }
 
     return count;
 }
+
